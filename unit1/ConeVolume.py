9adf3eff9fd7b3f1f8313ee772101160c795868e
Vaishnavi Prajapati <vaishnavipeb23@gmail.com>
2024-04-12 10:35:41 +0530

    commited

---
+++ unit1/ConeVolume.py
@@ -0,0 +1,6 @@
+print("enter a value of cone's radius : ")
+radius=int(input())
+print("enter a value of cone's height : ")
+height=int(input())
+volume=(3.14 * radius ** 2 * height)/3
+print("Volume of the cone is",volume,"metercube")
